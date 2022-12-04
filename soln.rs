47f5933bfe6b1c314d4ad10fd445cd73b017a6dc
Bart Massey <bart.massey@gmail.com>
2022-12-04 14:21:30 -0800

    cleaned up hashmap init a bit

--- soln.rs
+++ soln.rs
@@ -3,45 +3,48 @@
 // for license terms.
 
 //! Advent of Code Day 2.  
-//! Bart Massey 2022
+//! Ben and Bart Massey 2022
 
 use aoc::*;
 use std::collections::HashMap;
 
 fn main() {
-    let mut results = HashMap::new();
-
-    match get_part() {
-        Part1 => {
+    let scores = match get_part() {
+        Part1 => &[
             // A or X = rock
             // B or Y = paper
-            // C or Z = scizzors
-            results.insert("A X".to_string(), 3 + 1);
-            results.insert("A Y".to_string(), 6 + 2);
-            results.insert("A Z".to_string(), 0 + 3);
-            results.insert("B X".to_string(), 0 + 1);
-            results.insert("B Y".to_string(), 3 + 2);
-            results.insert("B Z".to_string(), 6 + 3);
-            results.insert("C X".to_string(), 6 + 1);
-            results.insert("C Y".to_string(), 0 + 2);
-            results.insert("C Z".to_string(), 3 + 3);
-        }
-        Part2 => {
+            // C or Z = scissors
+            ("A X", 3 + 1),
+            ("A Y", 6 + 2),
+            ("A Z", 0 + 3),
+            ("B X", 0 + 1),
+            ("B Y", 3 + 2),
+            ("B Z", 6 + 3),
+            ("C X", 6 + 1),
+            ("C Y", 0 + 2),
+            ("C Z", 3 + 3),
+        ],
+        Part2 => &[
             // A = rock, X = lose
             // B = paper, Y = tie
-            // C = scizzors, Z = win
-            results.insert("A X".to_string(), 0 + 3);
-            results.insert("A Y".to_string(), 3 + 1);
-            results.insert("A Z".to_string(), 6 + 2);
-            results.insert("B X".to_string(), 0 + 1);
-            results.insert("B Y".to_string(), 3 + 2);
-            results.insert("B Z".to_string(), 6 + 3);
-            results.insert("C X".to_string(), 0 + 2);
-            results.insert("C Y".to_string(), 3 + 3);
-            results.insert("C Z".to_string(), 6 + 1);
-        }
-    }
+            // C = scissors, Z = win
+            ("A X", 0 + 3),
+            ("A Y", 3 + 1),
+            ("A Z", 6 + 2),
+            ("B X", 0 + 1),
+            ("B Y", 3 + 2),
+            ("B Z", 6 + 3),
+            ("C X", 0 + 2),
+            ("C Y", 3 + 3),
+            ("C Z", 6 + 1),
+        ],
+    };
     
+    let results: HashMap<String, u64> = scores
+        .iter()
+        .map(|&(desc, score)| (desc.to_string(), score))
+        .collect();
+
     let mut points = 0u64;
     for line in input_lines() {
         if line.is_empty() {
