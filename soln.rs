0700713feb84a908097cd0269daf1e4481fe7e09
Benjamin Massey <benjamin.w.massey@gmail.com>
2022-12-04 11:32:26 -0800

    Part 1 done: verified answer

--- soln.rs
+++ soln.rs
@@ -6,10 +6,41 @@
 //! Bart Massey 2022
 
 use aoc::*;
+use std::collections::HashMap;
 
 fn main() {
+    // A or X = rock
+    // B or Y = paper
+    // C or Z = scizzors
+    let mut results = HashMap::new();
+    results.insert("A X".to_string(), 3 + 1);
+    results.insert("A Y".to_string(), 6 + 2);
+    results.insert("A Z".to_string(), 0 + 3);
+    results.insert("B X".to_string(), 0 + 1);
+    results.insert("B Y".to_string(), 3 + 2);
+    results.insert("B Z".to_string(), 6 + 3);
+    results.insert("C X".to_string(), 6 + 1);
+    results.insert("C Y".to_string(), 0 + 2);
+    results.insert("C Z".to_string(), 3 + 3);
+
     match get_part() {
-        Part1 => todo!(),
+        Part1 => {
+            let mut points = 0u64;
+            for line in input_lines() {
+                if line.is_empty() {
+                    eprintln!("empty line: unexpected, skipping");
+                }
+                else {
+                    if results.contains_key(&line) {
+                        points += results[&line];
+                    }
+                    else {
+                        eprintln!("unexpected line '{line}', skipping");
+                    }
+                }
+            }
+            println!("{points}");
+        }
         Part2 => todo!(),
     }
 }
