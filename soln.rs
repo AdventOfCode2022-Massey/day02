6a93c502ab347b849339819c8036d3add21d0e81
Benjamin Massey <benjamin.w.massey@gmail.com>
2022-12-04 11:42:24 -0800

    Finished and verified part 2, slight organization affecting part 1

--- soln.rs
+++ soln.rs
@@ -9,38 +9,52 @@ use aoc::*;
 use std::collections::HashMap;
 
 fn main() {
-    // A or X = rock
-    // B or Y = paper
-    // C or Z = scizzors
     let mut results = HashMap::new();
-    results.insert("A X".to_string(), 3 + 1);
-    results.insert("A Y".to_string(), 6 + 2);
-    results.insert("A Z".to_string(), 0 + 3);
-    results.insert("B X".to_string(), 0 + 1);
-    results.insert("B Y".to_string(), 3 + 2);
-    results.insert("B Z".to_string(), 6 + 3);
-    results.insert("C X".to_string(), 6 + 1);
-    results.insert("C Y".to_string(), 0 + 2);
-    results.insert("C Z".to_string(), 3 + 3);
 
     match get_part() {
         Part1 => {
-            let mut points = 0u64;
-            for line in input_lines() {
-                if line.is_empty() {
-                    eprintln!("empty line: unexpected, skipping");
-                }
-                else {
-                    if results.contains_key(&line) {
-                        points += results[&line];
-                    }
-                    else {
-                        eprintln!("unexpected line '{line}', skipping");
-                    }
-                }
-            }
-            println!("{points}");
+            // A or X = rock
+            // B or Y = paper
+            // C or Z = scizzors
+            results.insert("A X".to_string(), 3 + 1);
+            results.insert("A Y".to_string(), 6 + 2);
+            results.insert("A Z".to_string(), 0 + 3);
+            results.insert("B X".to_string(), 0 + 1);
+            results.insert("B Y".to_string(), 3 + 2);
+            results.insert("B Z".to_string(), 6 + 3);
+            results.insert("C X".to_string(), 6 + 1);
+            results.insert("C Y".to_string(), 0 + 2);
+            results.insert("C Z".to_string(), 3 + 3);
         }
-        Part2 => todo!(),
+        Part2 => {
+            // A = rock, X = lose
+            // B = paper, Y = tie
+            // C = scizzors, Z = win
+            results.insert("A X".to_string(), 0 + 3);
+            results.insert("A Y".to_string(), 3 + 1);
+            results.insert("A Z".to_string(), 6 + 2);
+            results.insert("B X".to_string(), 0 + 1);
+            results.insert("B Y".to_string(), 3 + 2);
+            results.insert("B Z".to_string(), 6 + 3);
+            results.insert("C X".to_string(), 0 + 2);
+            results.insert("C Y".to_string(), 3 + 3);
+            results.insert("C Z".to_string(), 6 + 1);
+        }
     }
+    
+    let mut points = 0u64;
+    for line in input_lines() {
+        if line.is_empty() {
+            eprintln!("empty line: unexpected, skipping");
+        }
+        else {
+            if results.contains_key(&line) {
+                points += results[&line];
+            }
+            else {
+                eprintln!("unexpected line '{line}', skipping");
+            }
+        }
+    }
+    println!("{points}");
 }
