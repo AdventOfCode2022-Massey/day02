782cb42c4d4eb23c6f2ccdc8d91c7d2c820257ed
Bart Massey <bart.massey@gmail.com>
2022-12-04 13:09:20 -0800

    finished part 1

--- soln.rs
+++ soln.rs
@@ -6,10 +6,35 @@
 //! Bart Massey 2022
 
 use aoc::*;
+use aoc_reparse::*;
+
+fn outcome(opp: u32, me: u32) -> u32 {
+    if opp == me {
+        3
+    } else if (opp + 1) % 3 == me {
+        6
+    } else {
+        0
+    }
+}
 
 fn main() {
+    let parser = Reparse::new(r"([ABC]) ([XYZ])");
+    let strategy = input_lines()
+        .map(|line| {
+            let parsed = parser.parse(&line).unwrap();
+            (
+                parsed.get::<char>(1) as u32 - 'A' as u32,
+                parsed.get::<char>(2) as u32 - 'X' as u32,
+            )
+        });
     match get_part() {
-        Part1 => todo!(),
+        Part1 => {
+            let score: u32 = strategy
+                .map(|(opp, me)| me + 1 + outcome(opp, me))
+                .sum();
+            println!("{score}");
+        }
         Part2 => todo!(),
     }
 }
